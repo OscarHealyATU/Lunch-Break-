c359cfd778cf1883f24b765c82de645745e46933
OscarHealyATU <g00424324@atu.ie>
2024-03-11 16:11:27 +0000

    adding stuff renaming cursor, removing spare cusror js scripts

---
+++ scripts/cards.js
@@ -0,0 +1,17 @@
+document.write(``);
+for (let i = 0; i < 10; i++) {
+  
+  document.write(`<div class="col-sm mt-5">
+  <div class="card" style="width: 18rem;">
+      <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+      </div>  
+</div>
+  
+`);
+
+}
